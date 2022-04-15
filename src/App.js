b6af17b7c6d9dff14c775dc21ea550b8d81febcc
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-04-15 13:07:20 +0100

    Changed the initialQuote

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 import DisplayQuote from "./components/displayQuote";
 
 function App() {
-  const [quote, setQuote] = useState([]);
+  const initialQuote = {
+    quote:
+      "Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?",
+    character: "Abe Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593",
+    characterDirection: "Right",
+  };
 
-  useEffect(() => {
-    async function fetchData() {
-      const request = await axios.get(
-        "https://simpsons-quotes-api.herokuapp.com/quotes"
-      );
-      setQuote(request.data[0]);
-    }
-    fetchData();
-  }, []);
+  const [quote, setQuote] = useState(initialQuote);
+
   const getQuote = () => {
     axios
       .get("https://simpsons-quotes-api.herokuapp.com/quotes")
